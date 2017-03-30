4454d24b9cd9adde4b66fe43ba951fd01f7dbad2
Laura Ramos <lauramilena.rbe@gmail.com>
2017-03-30 00:50:13 -0500

    funciona?

--- src/test/java/edu/eci/pdsw/samples/tests/AlquilerTest.java
+++ src/test/java/edu/eci/pdsw/samples/tests/AlquilerTest.java
@@ -33,23 +33,15 @@ import edu.eci.pdsw.sampleprj.dao.*;
 public class AlquilerTest {
     public AlquilerTest() {
     }
-    
-    @Inject
-    private ItemDAO daoItem;
-    @Inject
-    private ClienteDAO daoCliente;
-    @Inject
-    private TipoItemDAO daoTipoItem;
-    
-    
+        
     @Before
     public void setUp() {
     }
     
     @Test
     public void CF1Test() throws ExcepcionServiciosAlquiler{
-        ServiciosAlquiler sa=ServiciosAlquilerFactory.getInstance().getServiciosAlquilerTesting();
         /*
+        ServiciosAlquiler sa=ServiciosAlquilerFactory.getInstance().getServiciosAlquilerTesting();
         Item i1=new Item(sa.consultarTipoItem(1), 44, "Los 4 Fantasticos", "Los 4 Fantásticos  es una película de superhéroes  basada en la serie de cómic homónima de Marvel.", java.sql.Date.valueOf("2005-06-08"), 2000, "DVD", "Ciencia Ficcion");        
         sa.registrarCliente(new Cliente("Juan Perez",3842,"24234","calle 123","aa@gmail.com"));
         sa.registrarItem(i1);
